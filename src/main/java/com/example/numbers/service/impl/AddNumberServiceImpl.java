6e8498191baa085a54bc886cf9a8ebdd7b2053f4
unknown <sank.shetye@gmail.com>
2022-02-20 19:27:33 +0530

    Add Number - Negative Scenario

--- src/main/java/com/example/numbers/service/impl/AddNumberServiceImpl.java
+++ src/main/java/com/example/numbers/service/impl/AddNumberServiceImpl.java
@@ -1,17 +1,24 @@
 package com.example.numbers.service.impl;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Service;
 
 import com.example.numbers.service.AddNumberService;
 
 @Service
 public class AddNumberServiceImpl implements AddNumberService {
-			
+
 	public int addNumbers(String numbers) {
+		if(Objects.isNull(numbers) || numbers.length()==0) {
+			return 0;
+		}
 		String[] numbersArray = numbers.split(",");
 		int total = 0;
-		for(String number : numbersArray) {
-			total += Integer.parseInt(number);
+		if (Objects.nonNull(numbersArray) && numbersArray.length > 0) {
+			for (String number : numbersArray) {
+				total += Integer.parseInt(number);
+			}
 		}
 		return total;
 	}
--- src/test/java/com/example/numbers/service/impl/AddNumberServiceImplTest.java
+++ src/test/java/com/example/numbers/service/impl/AddNumberServiceImplTest.java
@@ -17,4 +17,22 @@ public class AddNumberServiceImplTest {
 		int total = addNumberServiceImpl.addNumbers("4,5");
 		Assert.assertEquals(9,total);
 	}
+	
+	@Test
+	public void testAddNumberBlankValue() {
+		int total = addNumberServiceImpl.addNumbers("");
+		Assert.assertEquals(0,total);
+	}
+
+	@Test
+	public void testAddNumberOneValue() {
+		int total = addNumberServiceImpl.addNumbers("1");
+		Assert.assertEquals(1,total);
+	}
+	
+	@Test
+	public void testAddNumberNullValue() {
+		int total = addNumberServiceImpl.addNumbers(null);
+		Assert.assertEquals(0,total);
+	}
 }
